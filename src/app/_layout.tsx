dd1a77ba133f3e69b87e3fbe9e2f3b0ee300dfa7
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-28 19:57:00 +0530

    CUSTOMER LOG-IN SCREEN ADDED

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,10 @@ const Layout = () => {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="role" />
+      <Stack.Screen name="customer/auth" />
+      <Stack.Screen name="captain/auth" />
+      <Stack.Screen name="customer/home" />
+      <Stack.Screen name="captain/home" />
     </Stack>
   );
 };
