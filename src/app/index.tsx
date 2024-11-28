2465a175358db03f29312dcbca5fd041e6029080
Abhik Baidya <abhikbaidya7@gmail.com>
2024-11-28 18:11:54 +0530

    SPLASH-SCREEN ADDED

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,44 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
 import { commonStyles } from "@/styles/commonStyles";
+import { splashStyles } from "@/styles/splashStyles";
+import CustomText from "@/components/shared/CustomText";
+import { useFonts } from "expo-font";
+import { resetAndNavigate } from "@/utils/Helpers";
 
 const Main = () => {
+  const [loaded] = useFonts({
+    Bold: require("../assets/fonts/NotoSans-Bold.ttf"),
+    Regular: require("../assets/fonts/NotoSans-Regular.ttf"),
+    Medium: require("../assets/fonts/NotoSans-Medium.ttf"),
+    Light: require("../assets/fonts/NotoSans-Light.ttf"),
+    SemiBold: require("../assets/fonts/NotoSans-SemiBold.ttf"),
+  });
+
+  const [hasNavigated, setHasNavigated] = useState(false);
+
+  const tokenCheck = async () => {
+    resetAndNavigate("/role");
+  };
+  useEffect(() => {
+    if (loaded && !hasNavigated) {
+      const timeoutId = setTimeout(() => {
+        tokenCheck();
+        setHasNavigated(true);
+      }, 1000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [loaded, hasNavigated]);
+
   return (
     <View style={commonStyles.container}>
-      <Text>Main</Text>
+      <Image
+        source={require("@/assets/images/logo_t.png")}
+        style={splashStyles.img}
+      />
+      <CustomText variant="h5" fontFamily="Medium" style={splashStyles.text}>
+        Made in 🇮🇳
+      </CustomText>
     </View>
   );
 };
