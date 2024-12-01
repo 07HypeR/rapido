380acbd760a7f5df8428e347feab347147350a4f
Abhik Baidya <abhikbaidya7@gmail.com>
2024-12-01 22:54:36 +0530

    RIDE-OPTION SELECTION FEATURE ADDED

--- src/app/customer/ridebooking.tsx
+++ src/app/customer/ridebooking.tsx
@@ -1,12 +1,128 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import React, { memo, useCallback, useMemo, useState } from "react";
+import { useRoute } from "@react-navigation/native";
+import { useUserStore } from "@/store/useStore";
+import { calculateFare } from "@/utils/mapUtils";
+import { rideStyles } from "@/styles/rideStyles";
+import { SystemBars } from "react-native-edge-to-edge";
+import CustomText from "@/components/shared/CustomText";
+import { commonStyles } from "@/styles/commonStyles";
 
 const RideBooking = () => {
+  const route = useRoute() as any;
+  const item = route?.params as any;
+  const { location } = useUserStore() as any;
+  const [selectedOption, setSelectedOption] = useState("Bike");
+  const [loading, setLoading] = useState(false);
+
+  const farePrices = useMemo(
+    () => calculateFare(parseFloat(item?.distanceInKm)),
+    [item?.distanceInKm]
+  );
+
+  const rideOptions = useMemo(
+    () => [
+      {
+        type: "Bike",
+        seats: 1,
+        time: "1 min",
+        dropTime: "4:28 pm",
+        price: farePrices?.bike,
+        isFastest: true,
+        icon: require("@/assets/icons/bike.png"),
+      },
+      {
+        type: "Auto",
+        seats: 3,
+        time: "1 min",
+        dropTime: "4:30 pm",
+        price: farePrices.auto,
+        icon: require("@/assets/icons/auto.png"),
+      },
+      {
+        type: "Cab Economy",
+        seats: 4,
+        time: "1 min",
+        dropTime: "4:28 pm",
+        price: farePrices.cabEconomy,
+        icon: require("@/assets/icons/cab.png"),
+      },
+      {
+        type: "Cab Premium",
+        seats: 4,
+        time: "1 min",
+        dropTime: "4:30 pm",
+        price: farePrices.cabPremium,
+        icon: require("@/assets/icons/cab_premium.png"),
+      },
+    ],
+    [farePrices]
+  );
+
+  const handleOptionSelect = useCallback((type: string) => {
+    setSelectedOption(type);
+  }, []);
+
   return (
-    <View>
-      <Text>RideBooking</Text>
+    <View style={rideStyles.container}>
+      <SystemBars style="light" />
+
+      <View style={rideStyles.rideSelectionContainer}>
+        <View style={rideStyles?.offerContainer}>
+          <CustomText fontSize={12} style={rideStyles.offerText}>
+            You get ₹10 off 5 coins cashback!
+          </CustomText>
+        </View>
+
+        <ScrollView
+          contentContainerStyle={rideStyles?.scrollContainer}
+          showsVerticalScrollIndicator={false}>
+          {rideOptions?.map((ride, index) => (
+            <RideOption
+              key={index}
+              ride={ride}
+              selected={selectedOption}
+              onSelect={handleOptionSelect}
+            />
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 };
 
-export default RideBooking;
+const RideOption = memo(({ ride, selected, onSelect }: any) => (
+  <TouchableOpacity
+    onPress={() => onSelect(ride?.type)}
+    style={[
+      rideStyles.rideOption,
+      { borderColor: selected === ride.type ? "#222" : "#ddd" },
+    ]}>
+    <View style={commonStyles.flexRowBetween}>
+      <Image source={ride?.icon} style={rideStyles?.rideIcon} />
+      <View style={rideStyles?.rideDetails}>
+        <CustomText fontFamily="Medium" fontSize={12}>
+          {ride?.type}{" "}
+          {ride?.isFastest && (
+            <Text style={rideStyles.fastestLabel}>FASTEST</Text>
+          )}
+        </CustomText>
+        <CustomText fontSize={10}>
+          {ride?.seats} seats • {ride?.time} away • Drop {ride?.dropTime}
+        </CustomText>
+      </View>
+      <View style={rideStyles?.priceContainer}>
+        <CustomText fontFamily="Medium" fontSize={14}>
+          ₹{ride?.price?.toFixed(2)}
+        </CustomText>
+        {selected === ride.type && (
+          <Text style={rideStyles?.discountedPrice}>
+            ₹{Number(ride?.price + 10).toFixed(2)}
+          </Text>
+        )}
+      </View>
+    </View>
+  </TouchableOpacity>
+));
+
+export default memo(RideBooking);
