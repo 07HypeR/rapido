d9fc183812fbfff2f0270f191f28dd60b5d7f4d3
Abhik Baidya <abhikbaidya7@gmail.com>
2024-12-04 11:48:34 +0530

    CAPTAIN LIVE-RIDE PAGE ADDED

--- src/service/rideService.tsx
+++ src/service/rideService.tsx
@@ -26,3 +26,23 @@ export const createRide = async (payload: {
     console.log("Error:Create Ride ", error);
   }
 };
+
+export const getMyRides = async (isCustomer: boolean = true) => {
+  try {
+    const res = await appAxios.get(`/ride/rides`);
+    const filterRides = res.data.rides?.filter(
+      (ride: any) => ride?.status != "COMPLETED"
+    );
+    if (filterRides?.length > 0) {
+      router?.navigate({
+        pathname: isCustomer ? "/customer/liveride" : "/captain/liveride",
+        params: {
+          id: filterRides![0]?._id,
+        },
+      });
+    }
+  } catch (error: any) {
+    Alert.alert("Oh! Dang there was an error");
+    console.log("Error:Get My Ride ", error);
+  }
+};
