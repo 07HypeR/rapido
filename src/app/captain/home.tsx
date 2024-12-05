27441c022302db42f53b2fd45e5d95fbec21165a
Abhik Baidya <abhikbaidya7@gmail.com>
2024-12-05 14:40:44 +0530

    CAPTAIN-HEADER ADDED

--- src/app/captain/home.tsx
+++ src/app/captain/home.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { getMyRides } from "@/service/rideService";
+import { homeStyles } from "@/styles/homeStyles";
+import { StatusBar } from "expo-status-bar";
+import CaptainHeader from "@/components/captain/CaptainHeader";
 
 const Home = () => {
+  useEffect(() => {
+    getMyRides(false);
+  }, []);
+
   return (
-    <View>
-      <Text>Captain</Text>
+    <View style={homeStyles.container}>
+      <StatusBar style="light" backgroundColor="orange" translucent={false} />
+      <CaptainHeader />
     </View>
   );
 };
